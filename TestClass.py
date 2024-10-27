eeee7c4846e38fa2f7fff7f65bc41f167d075636
Henrique Trivelato <102193625+Trivela90@users.noreply.github.com>
2024-10-27 16:25:53 -0300

    calcula.py + TestCalss.py
    calcula.py eh código python contendo todas as funções para a nossa calculadora. Já, TestClass.py é o código que será usado para PyTest

---
+++ TestClass.py
@@ -0,0 +1,171 @@
+from calcula import calculadora
+
+class TestClass:
+
+    # Testando +
+    def test_one(self):
+        elemento_1 = 1
+        elemento_2 = 2
+        operation = "+"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 3
+
+    # Testando -
+    def test_two(self):
+        elemento_1 = 1
+        elemento_2 = 2
+        operation = "-"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == -1
+
+    # Testando *
+    def test_three(self):
+        elemento_1 = 1
+        elemento_2 = 2
+        operation = "*"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 2
+
+    # Testando /
+    def test_four(self):
+        elemento_1 = 1
+        elemento_2 = 2
+        operation = "/"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 0.5
+
+    # Testando ^
+    def test_five(self):
+        elemento_1 = 1
+        elemento_2 = 2
+        operation = "^"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 1
+
+    # Testando número negativo (Eh pra dar -8 -> logo dá erro)
+    def test_six(self):
+        elemento_1 = -10
+        elemento_2 = 2
+        operation = "+"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 8
+
+    # Testando número decimal
+    def test_seven(self):
+        elemento_1 = 23.75
+        elemento_2 = 1.25
+        operation = "-"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 22.5
+
+    # Testando número decimal e negativo
+    def test_eight(self):
+        elemento_1 = -23.75
+        elemento_2 = 3
+        operation = "*"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == -71.25
+
+    # Testando divisão por 0
+    def test_nine(self):
+        elemento_1 = 75
+        elemento_2 = 0
+        operation = "/"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+        # Testando elemento 1 inválido
+    def test_ten(self):
+        elemento_1 = "a"
+        elemento_2 = 5.6
+        operation = "-"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando elemento 2 inválido
+    def test_eleven(self):
+        elemento_1 = 3.7
+        elemento_2 = "b"
+        operation = "+"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando sinal inválido
+    def test_twelve(self):
+        elemento_1 = 75
+        elemento_2 = 1
+        operation = "#"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando 2 elementos inválidos
+    def test_thirteen(self):
+        elemento_1 = "-23.4.5"
+        elemento_2 = "b"
+        operation = "+"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando elemento 1 inválido e sinal inválido
+    def test_fourteen(self):
+        elemento_1 = "-23.4.5"
+        elemento_2 = "2.3"
+        operation = ")"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando elemento 2 inválido e sinal inválido
+    def test_fiftteen(self):
+        elemento_1 = "-1.23"
+        elemento_2 = "-."
+        operation = "&"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando as 3 entradas inválidas
+    def test_sixteen(self):
+        elemento_1 = "123.hue"
+        elemento_2 = "-."
+        operation = "&"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == None
+
+    # Testando Exponenciação
+    def test_seventeen(self):
+        elemento_1 = -3
+        elemento_2 = 4
+        operation = "^"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == 81
+
+    # Testando se resultado do produto de floats é diferente do de int
+    def test_seventeen(self):
+        elemento_1 = 5.0
+        elemento_2 = 7.0
+        operation = "*"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == (5*7)
+
+    # Testando se resultado da divisão de floats é diferente da de int
+    def test_eighteen(self):
+        elemento_1 = 15.0
+        elemento_2 = 5.0
+        operation = "/"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == (15/5)
+
+    # Testando soma de negativo com posititvo
+    def test_nineteen(self):
+        elemento_1 = 7.5
+        elemento_2 = -8.25
+        operation = "+"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == -0.75
+
+    # Testando subtração de negativo com posititvo
+    def test_twenty(self):
+        elemento_1 = -7.5
+        elemento_2 = 23
+        operation = "-"
+        resultado = calculadora(elemento_1, elemento_2, operation)
+        assert resultado == -30.5
---
+++ calcula.py
@@ -0,0 +1,117 @@
+# Author:
+#   Henrique Trivelato de Angelo
+
+def negativeCheck(number):
+    if(number[0] == '-'):
+        return True
+    return False
+
+
+def segundaStringVazia(string):
+    if(string == ""):
+        return True
+    return False
+
+
+# -------------------------
+def numberCheck(number):
+    eh_negativo = negativeCheck(number)
+
+    if(eh_negativo == True):
+        number = number[1:]   # Retirando o sinal '-'
+        # print(number)
+
+    eh_numeric = number.isnumeric()
+
+    if(eh_numeric == False):
+
+        # Se for letra, retorna False (eh_numeric = False)
+        if(number.isalpha() == True):
+            return eh_numeric
+        
+        # Se não for numerico, quebra em 2 substrings a partir do char '.'
+        substrings = number.split(".", 1)   # quebra a String no maximo 1 vez (1 quebra = substrings)
+                                            #                                 (2 quebras = 3 substrings)
+
+        sub1_eh_numeric = substrings[0].isnumeric()
+        sub2_eh_numeric = substrings[1].isnumeric()
+
+        # Se a 2a string for vazia, logo eh um numero decimal
+        if(sub2_eh_numeric == False):
+            sub2_eh_numeric = segundaStringVazia(substrings[1])
+
+        # True -> É numero decimal
+        # False -> Não é número
+        return (sub1_eh_numeric and sub2_eh_numeric)
+    # Fim if()
+
+    # É numero (mas nao eh numero decimal)
+    return eh_numeric
+
+
+# -------------------------
+def checkOperacao(operacao):
+
+    if(operacao == "+"):
+        return True
+    
+    if(operacao == "-"):
+        return True
+    
+    if(operacao == "*"):
+        return True
+    
+    if(operacao == "/"):
+        return True
+    
+    if(operacao == "^"):
+        return True
+    
+    return False
+
+
+# -------------------------
+def calculadora(elemento_1, elemento_2, sinal):
+    val_1 = str(elemento_1)
+    val_2 = str(elemento_2)
+    operacao = str(sinal)
+
+    if(numberCheck(val_1) == False):
+        return None
+    
+    if(numberCheck(val_2) == False):
+        return None
+    
+    if(checkOperacao(operacao) == False):
+        return None
+    
+    # Operação e números válidos
+    var1 = float(val_1)
+    var2 = float(val_2)
+
+    if(operacao == "+"):
+        return var1 + var2
+    
+    if(operacao == "-"):
+        return var1 - var2
+    
+    if(operacao == "*"):
+        return var1 * var2
+    
+    if(operacao == "/"):
+
+        if(var2 == 0):
+            return None
+        return var1 / var2
+    
+    if(operacao == "^"):
+        return pow(var1,var2)
+    
+
+# -------- MAIN 
+# elemento_1 = input()
+# elemento_2 = input()
+# operation = input()
+
+# resultado = calculadora(elemento_1, elemento_2, operation)
+# print(resultado)
